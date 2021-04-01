6a5f9c487ab2187e8fa7437e71ec9c91b47a3646
navid <navidm@chalmers.se>
2021-04-01 11:28:54 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 29 09:38:52 2021
+
+@author: navid
+"""
+
+
+import numpy as np
+
+Q = [[0,0,0],[0,0,0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0,0]]
